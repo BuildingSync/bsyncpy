9fdbd7e75834dbc481ec471f6e815fb830aa275b
corymosiman12 <cory.mosiman12@gmail.com>
2021-02-25 15:03:29 -0500

    add test for int

--- tests/test_bsync.py
+++ tests/test_bsync.py
@@ -3,3 +3,7 @@ from bsync import bsync
 def test_initialize():
     b = bsync.BuildingSync()
     assert b is not None
+
+def test_int_datatype():
+    b = bsync.Sections.Section.Story(1)
+    assert b is not None
